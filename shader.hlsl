e7489da38d86487a605a00a13d03f297658caaf8
jesselai <jesselai@tencent.com>
2021-11-29 22:03:32 +0800

    tweaks

--- shader.hlsl
+++ shader.hlsl
@@ -1,3 +1,20 @@
+
+cbuffer Projection : register(b0)
+{
+	float4x4 GProjectionMatrix;
+};
+
+cbuffer cbMtl : register(b0)
+{
+    float4 BaseColor;
+    float Roughness;
+    float Metalic;
+    float Specular;
+    float Alpha;
+    float Emissive;
+}
+
+
 float4 VS(float4 position : POSITION) : SV_POSITION
 {
 	return position;
